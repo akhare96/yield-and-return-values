ee0df9415a3c7ecf9e7a0b4325c6e4b320d5bb18
Ambar Khare <ambar.khare24@gmail.com>
2021-07-20 10:45:08 -0700

    commit tree efff50e8097f3cf2fdfb7123a5d4faad1a596fee

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,14 @@
 require 'pry'
 
 def hello(array)
+  if
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
 end
-
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
